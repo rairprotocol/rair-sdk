97f8f8fec5ba299a91621d279010d5aa9e627eef
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2024-11-14 15:34:26 -0400

    Files endpoints

--- src/API/files.ts
+++ src/API/files.ts
@@ -1,14 +1,51 @@
 import Api from '../common/Api';
+import { ApiResponse, DatabaseId, FileId, Methods } from '../types/common';
+import {
+  UpdateMediaParams,
+  DeleteMediaResult,
+  ListMediaParams,
+  ListMediaResult,
+  GetFileByIdResult,
+  UpdateFileByIdParams,
+  GetFilesByCategoryParams,
+  GetFilesByCategoryResult,
+  GetFilesByTokenResult,
+  GetOffersByFileResult,
+  ConnectOfferAndFileParams,
+  ConnectOfferAndFileResult,
+  RemoveOfferFromFileParams,
+  RemoveOfferFromFileResult
+} from '../types/files';
 
 export class FilesAPI extends Api {
-  updateMedia() {}
-  deleteMedia() {}
-  listMedia() {}
-  getFileById() {}
-  updateFileById() {}
-  getFilesByCategory() {}
-  getFilesByToken() {}
-  getOffersByFile() {}
-  connectOfferAndFile() {}
-  removeOfferFromFile() {}
+  async updateMedia({id, ...bodyParams}: UpdateMediaParams) : Promise<ApiResponse> {
+    return this.apiCall(`files/${id}`, bodyParams, {}, Methods.patch);
+  }
+  async deleteMedia({id}: FileId) : Promise<DeleteMediaResult> {
+    return this.apiCall(`files/${id}`, {}, {}, Methods.delete);
+  }
+  async listMedia(params: ListMediaParams) : Promise<ListMediaResult> {
+    return this.apiCall('files/list', params, {}, Methods.patch);
+  }
+  async getFileById({id}: FileId) : Promise<GetFileByIdResult> {
+    return this.apiCall(`files/byId/${id}`);
+  }
+  async updateFileById({id, ...bodyParams}: UpdateFileByIdParams) : Promise<ApiResponse> {
+    return this.apiCall(`files/byId/${id}`, bodyParams, {}, Methods.put);
+  }
+  async getFilesByCategory({id, ...queryParams}: GetFilesByCategoryParams) : Promise<GetFilesByCategoryResult> {
+    return this.apiCall(`files/byCategory/:${id}`, {}, queryParams);
+  }
+  async getFilesByToken({id}: DatabaseId) : Promise<GetFilesByTokenResult> {
+    return this.apiCall(`files/forToken/${id}`);
+  }
+  async getOffersByFile({id}: FileId) : Promise<GetOffersByFileResult> {
+    return this.apiCall(`files/${id}/unlocks`);
+  }
+  async connectOfferAndFile({id, ...bodyParams}: ConnectOfferAndFileParams) : Promise<ConnectOfferAndFileResult> {
+    return this.apiCall(`files/${id}/unlocks`, bodyParams, {}, Methods.post);
+  }
+  async removeOfferFromFile({id, ...bodyParams}: RemoveOfferFromFileParams) : Promise<RemoveOfferFromFileResult> {
+    return this.apiCall(`files/${id}/unlocks`, bodyParams, {}, Methods.delete);
+  }
 }
---
+++ src/types/files.ts
@@ -0,0 +1,81 @@
+import { ApiResponse, FileId, FilterAndSortParams, PaginationParams } from "./common";
+import { Contract, MediaFile, Offer, Unlock } from "./database";
+
+export interface UpdateMediaParams extends FileId {
+  title?: string;
+  description?: string;
+  contract?: string;
+  product?: string;
+  offer?: Array<string>;
+  category?: string;
+  demo?: boolean;
+}
+export interface DeleteMediaResult extends ApiResponse {
+  message: string;
+}
+export interface ListMediaParams extends FilterAndSortParams, PaginationParams, Pick<
+    MediaFile,
+      'title' |
+      'description' |
+      'uploader' |
+      'ageRestricted' |
+      'demo'
+    > {
+  _id?: string;
+  author?: string;
+  contract?: string;
+  product?: string;
+  offer?: Array<string>
+}
+
+export interface ListMediaResult extends ApiResponse {
+  list: Array<Unlock>;
+  totalNumber: number;
+}
+
+export interface GetFileByIdResult extends ApiResponse {
+  file: Omit<MediaFile, 'key'>;
+}
+
+export interface UpdateFileByIdParams extends FileId, Pick<
+  MediaFile,
+    'title' |
+    'description' |
+    'uploader' |
+    'ageRestricted' |
+    'demo'
+  >
+{}
+
+export interface GetFilesByCategoryParams extends FileId, PaginationParams {}
+
+export interface GetFilesByCategoryResult extends ApiResponse {
+  results: number;
+  data: Array<MediaFile>;
+}
+
+export interface GetFilesByTokenResult extends ApiResponse {
+  results: number;
+  data: Array<MediaFile>;
+}
+
+export interface GetOffersByFileResult extends ApiResponse {
+  data: Array<
+          Omit<Unlock, 'offers'> & {
+            offers: Omit<Offer, 'contract'> & {
+              contract: Contract
+            };
+        }>;
+}
+export interface ConnectOfferAndFileParams extends FileId {
+  offers: Array<string>;
+}
+export interface ConnectOfferAndFileResult extends ApiResponse {
+  offer: Unlock
+}
+export interface RemoveOfferFromFileParams extends FileId {
+  offer: string;
+}
+export interface RemoveOfferFromFileResult extends ApiResponse {
+  offer: Unlock
+}
