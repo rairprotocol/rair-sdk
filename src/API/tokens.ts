6f3a5f1f2ef920da912d3d50f8cfae981b14bafb
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2024-11-21 11:06:38 -0400

    Tokens

--- src/API/tokens.ts
+++ src/API/tokens.ts
@@ -1,7 +1,21 @@
 import Api from '../common/Api';
+import { DatabaseId } from '../types/common';
+import {
+  GetAllTokensParams,
+  GetAllTokensResult,
+  GetSingleTokenParams,
+  GetSingleTokenResult,
+  GetFullTokenResult
+} from '../types/tokens';
 
 export class TokensAPI extends Api {
-  getAllTokens() {}
-  getSingleToken() {}
-  getFullTokenInfo() {}
+  async getAllTokens(params: GetAllTokensParams) : Promise<GetAllTokensResult> {
+    return this.apiCall('', {}, params);
+  }
+  async getSingleToken({token, ...params}: GetSingleTokenParams) : Promise<GetSingleTokenResult> {
+    return this.apiCall(`${token}`, {}, params);
+  }
+  async getFullTokenInfo({id}: DatabaseId) : Promise<GetFullTokenResult> {
+    return this.apiCall(`id/${id}`);
+  }
 }
---
+++ src/types/tokens.ts
@@ -0,0 +1,27 @@
+import { ApiResponse, PaginationParams } from "./common";
+import { Contract, MintedToken, Offer, Product, User } from './database';
+
+export interface GetAllTokensParams extends PaginationParams, MintedToken {}
+export interface GetAllTokensResult extends ApiResponse {
+    results: number;
+    tokens: Array<MintedToken>;
+}
+export interface GetSingleTokenParams {
+    contract: string;
+    offerPool?: string;
+    offers?: string;
+    token: string;
+}
+export interface GetSingleTokenResult extends ApiResponse {
+    data: {
+        doc: MintedToken;
+    }
+}
+export interface GetFullTokenResult extends ApiResponse {
+    tokenData: MintedToken & {
+        contract: Contract,
+        offer: Offer,
+        product: Product,
+        ownerData: User,
+    }
+}
