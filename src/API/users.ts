bad3f19fb894114b577f78e7167a7d960d38e4d3
Juan Miguel Sanchez Mola <jmsm412@gmail.com>
2025-06-11 10:32:11 -0400

    User routes cleanup

--- src/API/users.ts
+++ src/API/users.ts
@@ -45,38 +45,38 @@ export class UsersAPI extends Api {
   }
 
   async getReferencesForUser({userAddress}: UserAddress) : Promise<GetReferencesResponse> {
-    return this.apiCall(`/reference/${userAddress}`);
+    return this.apiCall(`reference/${userAddress}`);
   }
   async createReference(params: Omit<UserReferences, 'gitHandle'>) : Promise<CreateUserReferenceResponse> {
-    return this.apiCall(`/reference`, params, {}, Methods.post);
+    return this.apiCall(`reference`, params, {}, Methods.post);
   }
   async updateReference({id, ...bodyParams}: DatabaseId & Omit<UserReferences, 'gitHandle'>) : Promise<UpdateReferenceResponse> {
-    return this.apiCall(`/reference/${id}`, bodyParams, {}, Methods.put);
+    return this.apiCall(`reference/${id}`, bodyParams, {}, Methods.put);
   }
   async deleteReference({id}: DatabaseId) : Promise<ApiResponse> {
-    return this.apiCall(`/reference/${id}`, {}, {}, Methods.delete);
+    return this.apiCall(`reference/${id}`, {}, {}, Methods.delete);
   }
 
   async getPastProjectForUser({userAddress}: UserAddress) : Promise<GetPastProjectsResponse> {
-    return this.apiCall(`/experience/${userAddress}`);
+    return this.apiCall(`experience/${userAddress}`);
   }
   async createPastProject(params: Omit<UserProjects, 'gitHandle'>) : Promise<CreatePastProjectResponse> {
-    return this.apiCall(`/experience`, params, {}, Methods.post);
+    return this.apiCall(`experience`, params, {}, Methods.post);
   }
   async updatePastProject({id, ...bodyParams}: DatabaseId & Omit<UserProjects, 'gitHandle'>) : Promise<UpdatePastProjectResponse> {
-    return this.apiCall(`/experience/${id}`, bodyParams, {}, Methods.put);
+    return this.apiCall(`experience/${id}`, bodyParams, {}, Methods.put);
   }
   async deletePastProject({id}: DatabaseId) : Promise<ApiResponse> {
-    return this.apiCall(`/experience/${id}`, {}, {}, Methods.delete);
+    return this.apiCall(`experience/${id}`, {}, {}, Methods.delete);
   }
 
   async getAchivementData({userAddress}: UserAddress) : Promise<GetAchievementDataResponse> {
-    return this.apiCall(`/achievement/${userAddress}`);
+    return this.apiCall(`achievement/${userAddress}`);
   }
   async getUserValue({userAddress, namespace, value}: UserAddress & UserValueParam) : Promise<GetUserValueResponse> {
-    return this.apiCall(`/${userAddress}/${namespace}/${value}`);
+    return this.apiCall(`${userAddress}/${namespace}/${value}`);
   }
   async setUserValue({userAddress, namespace, value, }: UserAddress & UserValueParam) : Promise<SetUserValueResponse> {
-    return this.apiCall(`/${userAddress}/${namespace}/${value}`, {}, {}, Methods.post);
+    return this.apiCall(`${userAddress}/${namespace}/${value}`, {}, {}, Methods.post);
   }
 };
